a711ebf9c8faac01b195fcbe7e387bb8b3b7fec9
davidwu@ucsd.edu <davidwu@ucsd.edu>
2018-01-17 21:52:20 -0800

    added javascript

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,7 +11,16 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
-	});
+		$("a.thumbnail").click(projectClick);
+	}
+
+	function projectClick(e) { 
+    // prevent the page from reloading 
+    e.preventDefault();
+    // In an event handler, $(this) refers to 
+    // the object that triggered the event 
+    $(this).css("background-color", "#7fff00");
+}
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
